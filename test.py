b0d3b64aeae6440f78fc5edda907d55d695d5ade
X1_INTERNEな <72628651+Rayen2007@users.noreply.github.com>
2021-01-04 19:54:17 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import os 
+os.system( 'git pull' )
+os.system( 'clear' )
+
+pw = 'X1_internet20062006WX0''
+
+i  = input("Enter your passwourd")
+
+if i ==pw:
+	prinet("welcom")
+	else:
+		print("wrong passwourd ! ")
+		
